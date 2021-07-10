7cf912ca4487b9543eb0973773ebcea51cb95789
lazi19 <koaichetassia@yahoo.fr>
2021-07-10 11:12:50 +0200

    Rajout le programme qui gère la suppression de l'image de application modifySauce

--- backend/middleware/verifyPassword.js
+++ backend/middleware/verifyPassword.js
@@ -6,7 +6,7 @@ const passwordSchema = require('../models/password');
 module.exports = (req, res, next) => {
     if (!passwordSchema.validate(req.body.password)) {
         //return res.status(400,"8 caratères minimun").send({message: 'Mot de passe pas assez fort ! ' + passwordSchema.validate(req.body.password, {list:true})});
-        res.writeHead(400, '{"message":"Mot de passe requis : 8 caractères minimun. Au moins 1 Majuscule, 1 minuscule. Sans espaces"}', {
+        res.writeHead(400, '{"message":"Mot de passe requis : 8 caractères minimun. Au moins 1 Majuscule, 1 minuscule, au moins un chiffre. Sans espaces"}', {
             'content-type': 'application/json'
         });
         res.end('Format de mot de passe incorrect');
